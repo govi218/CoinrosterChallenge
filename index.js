9264da3d3ae4933b5c8f5c96f892714f83adec85
Govind Mohan <govi218mu@gmail.com>
2018-04-07 02:44:47 -0400

    first commit

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 bcypher = require('blockcypher');
 
-var bcapi = new bcypher('btc','test3','dea4a0d80c0a4567a758e2f6daa49050');
+var bcapi = new bcypher('btc','test3', 'dea4a0d80c0a4567a758e2f6daa49050');
 
 function printResponse(err, data) {
   if (err !== null) {
@@ -9,5 +9,6 @@ function printResponse(err, data) {
     console.log(data);
   }
 }
-bcapi.createWallet({name: 'Alice', addresses:['mqJyQypCxG7hj8nrTyN1KbJRsJiu5WaKv9']}, printResponse);
-bcapi.getAddrBal('mqJyQypCxG7hj8nrTyN1KbJRsJiu5WaKv9', {omitWalletAddresses: true}, printResponse);
+bcapi.genAddr({}, printResponse);
+
+bcapi.getAddrBal('mhVuC3GQzg7vBWqKU89QBZQy5xXC23SmTQ', {omitWalletAddresses: true}, printResponse);
